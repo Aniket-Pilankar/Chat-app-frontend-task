2e395eef4133022643867b9eedf6bf892dd64ba6
Aniket Pilankar <aniketpilankar1998@gmail.com>
2023-10-24 10:52:00 +0530

    update baseUrl with deployed link

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const request = axios.create({
-  baseURL: 'http://localhost:4001',
+  // baseURL: 'http://localhost:4001',
+  baseURL: 'https://chat-app-backend-js.onrender.com',
   headers: {
     'Content-type': 'application/json',
   },
-})
+});
 
-export default request
+export default request;
